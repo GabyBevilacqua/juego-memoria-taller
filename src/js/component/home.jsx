8d670caef543bc0cb9ed0caaf87b9ea8a1ae650a
GabyBevilacqua <174345066+GabyBevilacqua@users.noreply.github.com>
2024-11-03 17:49:08 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/js/component/home.jsx
@@ -0,0 +1,91 @@
+import React, { useEffect, useState } from "react";
+import Swal from 'sweetalert2'
+
+const Home = () => {
+
+	const cardData = [
+		{ id: 1, content: '🦥', matched: false },
+		{ id: 2, content: '🦥', matched: false },
+		{ id: 3, content: '🐶', matched: false },
+		{ id: 4, content: '🐶', matched: false },
+		{ id: 5, content: '🦆', matched: false },
+		{ id: 6, content: '🦆', matched: false },
+		{ id: 7, content: '🦢', matched: false },
+		{ id: 8, content: '🦢', matched: false },
+		{ id: 9, content: '🐉', matched: false },
+		{ id: 10, content: '🐉', matched: false },
+		{ id: 11, content: '🐟', matched: false },
+		{ id: 12, content: '🐟', matched: false },
+	]
+
+	const [selected, setSelected] = useState([])
+	const [cards, setCards] = useState()
+
+	const shuffle = () => cardData.sort(() => Math.random() - 0.5)
+
+	useEffect(() => {
+		setCards(prev => prev = shuffle())
+	}, [])
+	useEffect(() => {
+		if (selected.length === 2) {
+			if (selected[0].content === selected[1].content) {
+				selected[0].matched = true;
+				selected[1].matched = true;
+
+			}
+			setTimeout(() => {
+				let aux = 0;
+				cards.forEach(element => {
+					element.matched ? aux++ : ''
+					aux === cards.length && Swal.fire({
+						title: "Bien hecho!",
+						text: "Las encontraste todas!",
+						icon: "success"
+					});
+				})
+
+			}, 0)
+			setTimeout(() => {
+				setSelected(prev => prev = []);
+			}, 800)
+		}
+	}, [selected])
+
+
+	const handleClick = (card) => {
+		if (selected.length < 2 && !selected.includes(card)) {
+			setSelected(prev => prev = [...prev, card])
+		}
+	}
+
+	const handlePlayAgain = () => {
+		setCards(prev => prev = shuffle())
+		setSelected(prev => prev = []);
+	}
+
+	return (
+		<>
+			<div className="card col-4 mx-auto" id="title-card">
+				<div className="card-body">
+					<strong>Juego de memoria.</strong>
+				</div>
+			</div>
+			<div className="memory-game row">
+				{cards && cards.map(card =>
+					<div
+						key={card.id}
+						className={`col-4 card ${selected.includes(card) || card.matched ? 'visible flip-in-hor-bottom' : 'wobble-hor-bottom'}`}
+						onClick={() => handleClick(card)}
+					>
+						{selected.includes(card) || card.matched ? card.content : "❔"}
+
+					</div>)}
+				<button type="button" className="btn btn-success" onClick={handlePlayAgain}>
+					Juega Otra Vez!
+				</button>
+			</div>
+		</>
+	);
+};
+
+export default Home;
